3020be6d64312b78a34b9fde0626ee6e95b9c62a
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-08-09 13:59:11 +0900

    0809 13:58 ItemDataアタッチ済み

--- Assets/Item/ItemData.cs
+++ Assets/Item/ItemData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// アイテムの種類
+// アイテムの種類 //オリジナル型の作成 ItemType型の作成（決まった情報を持っているだけ、羅列しているだけの型）列挙型
 public enum ItemType
 {
     arrow,      // 矢
@@ -29,7 +29,8 @@ public class ItemData : MonoBehaviour
     {
         
     }
-    //接触
+
+    //接触　プレイヤーに拾われた時
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
@@ -37,12 +38,12 @@ public class ItemData : MonoBehaviour
             if (type == ItemType.GoldKey)
             {
                 //金のカギ
-                ItemKeeper.hasGoldKeys += 1;
+                ItemKeeper.hasGoldKeys += count;
             }
             else if (type == ItemType.Silverkey)
             {
                 //銀のカギ
-                ItemKeeper.hasSilverKeys += 1;
+                ItemKeeper.hasSilverKeys += count;
             }
             else if (type == ItemType.arrow)
             {
@@ -65,9 +66,11 @@ public class ItemData : MonoBehaviour
             else if (type == ItemType.light)
             {
                 //ライト
-                ItemKeeper.hasLights += 1;
+                ItemKeeper.hasLights += count;
+                //PlayerLightControllerスクリプトが持っているLightUpdate()メソッドの発動
                 GameObject.FindObjectOfType<PlayerLightController>().LightUpdate();
             }
+
             //アイテム取得演出
             // 当たりを消す
             gameObject.GetComponent<CircleCollider2D>().enabled = false;
